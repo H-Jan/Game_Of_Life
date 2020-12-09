1b25d4f33cc27f81d55bb4da03d5bfbde454afb6
Hani Jandali <hanijandali@Hanis-MacBook-Pro.local>
2020-12-09 11:28:14 -0800

    full classes included

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class UniversityPerson:
 
 #Child Class for Professors, will inherit the Super Class of the Parent
 class Professor(UniversityPerson):
-    def __init__(self, first_name, last_name, school_email, password, id_number)
+    def __init__(self, first_name, last_name, school_email, password, id_number):
         super().__init__(first_name, last_name, school_email, password, id_number)
         #Inheriting from the super class these attributes into the Professor Class 
         self.courses = []
@@ -61,7 +61,8 @@ class Student(UniversityPerson):
         for i, course in enumerate(course_list):
             print("Your assignments and greades for " + course + " are: ")
             #cycling through the list of courses 
-            if self.courses[i].assignments != [] #ensure that assignments is presented
+            if self.courses[i].assignments != []: 
+            #ensure that assignments is presented
                 for assignment, grade in zip(self.courses[i].assignments, self.grades):
                     print(assignment.name + " - " + str(grade.grade.getGrade()))
             else: 
@@ -75,7 +76,50 @@ class Courses:
         self.teacher = teacher
         self.assignments = []
 
+class Assignment:
+    min = 0
+    max = 100
+
+    def __init__(self, name, due_date, description):
+        self.name = name
+        self.due_date = due_date
+        self.description = description
+
+    def Name(self, name):
+        if name != "":
+            return self.name 
+
+    def DueDate(self, due_date):
+        if due_date != "":
+            return self.due_date
     
+    def Description(self, description):
+        if description != "":
+            return self.description
+
+class AssignmentGrades: 
+    def __init__(self, course, assignment, grade, grade_percentage):
+        self.course = course
+        self.assignment = assignment
+        self.grade = Grading(grade)
+        self.grade_percentage = grade_percentage
+
+    def Percentage(self, percentage):
+        if percentage != "":
+            return self.Percentage
+    
+class Grading:
+    min = 0
+    max = 100
+
+    def __init__(self, grade):
+        self.grade = grade
+
+    def Grade(self, grade):
+        while grade == "" or grade < Grading.min or grade > Grading.max:
+            grade = int(input("Incorrect grade, try again"))
+        self.grade = grade
+        return True
 
 
 
