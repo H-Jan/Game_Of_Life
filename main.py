ddc94fb4aebc5b83a0ca24862faec7a515fe82bd
Hani Jandali <hanijandali@Hanis-MacBook-Pro.local>
2020-12-08 21:49:14 -0800

    New Program for School Login, Super Class formation

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Testing")
+
+#New Program Take 3
+
+#SuperClass / Parent Class
+class Person:
+    def __init__(self, first_name, last_name, school_email, password, id_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.school_email = school_email
+        self.password = password
+        self.id_number = id_number
+
+    def FirstName(self, first_name):
+        if first_name != "":
+            return self.first_name
+        
+    def LastName(self, last_name):
+        if last_name != "":
+            return self.last_name
+
+    def SchoolEmail(self, school_email):
+        if school_email != "":
+            return self.school_email
+    
+    def Password(self, password):
+        if password != "":
+            return self.password
+    
+    def Identification(self, id_number):
+        if id_number != "":
+            return self.id_number
